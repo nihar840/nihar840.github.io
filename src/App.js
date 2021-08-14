2615b3f3700d79fb574173d495c9a756cfa7174e
Nihar Ranjan <nrmahajan840@gmail.com>
2021-08-14 19:43:37 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react';
 import './App.css';
 import Header from './Header/Header';
@@ -5,15 +6,21 @@ import Header from './Header/Header';
 function App() {
   return (
     <React.Fragment>
-    <header><Header/></header>
-    <body>
+    <Header/>
+    <div>
     <div className="App">
       <img className="profilePic" src="Images/Profile pic.jpg" alt=""/>
       <div className="myName">
         Nihar Ranjan
       </div>
+      <div>
+        <a href="https://twitter.com/NiharMahajan" className="fa fa-twitter"></a>
+        <a href="https://www.facebook.com/niharranjan.mahajan420" className="fa fa-facebook"></a>
+        <a href="https://www.linkedin.com/in/nihar-ranjan-5bb54853/" className="fa fa-linkedin"></a>
+        <a href="https://www.instagram.com/n_i_h_a_r___/" className="fa fa-instagram"></a>
+      </div>
     </div>
-    </body>
+    </div>
     </React.Fragment>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,9 +3,13 @@ import './Header.css';
 
  function Header(){
     return(
+        <React.Fragment>
         <div className = "header">
             <div className="logo">My Portfolio</div>
+            <div className="logoRight">Developed With React</div><img className="reactLogo" src="logo192.png" alt=""/>
+            <div className="animatingDiv"/>
         </div>
+        </React.Fragment>
     );
 }
 
